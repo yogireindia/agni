009c6b3ec1911c1e829d2a742d750a0b76fdfead
yogireindia <wacshortstudios@gmail.com>
2025-02-19 01:14:45 +0530

    Update agni.js

--- agni.js
+++ agni.js
@@ -1,13 +1,3 @@
-document.getElementById('contact-link').addEventListener('click', function (e) {
-    e.preventDefault(); // Prevent the default anchor behavior
-    document.getElementById('contactus').scrollIntoView({ behavior: 'smooth' });
-});
-
-document.getElementById('about-link').addEventListener('click', function (e) {
-    e.preventDefault(); // Prevent the default anchor behavior
-    document.getElementById('mukulsection').scrollIntoView({ behavior: 'smooth' });
-});
-
 const menuToggle = document.getElementById('menu-toggle');
     const mobileMenu = document.getElementById('mobile-menu');
 
@@ -29,6 +19,15 @@ const menuToggle = document.getElementById('menu-toggle');
         }
     });
 
+document.getElementById('contact-link').addEventListener('click', function (e) {
+    e.preventDefault(); // Prevent the default anchor behavior
+    document.getElementById('contactus').scrollIntoView({ behavior: 'smooth' });
+});
+
+document.getElementById('about-link').addEventListener('click', function (e) {
+    e.preventDefault(); // Prevent the default anchor behavior
+    document.getElementById('mukulsection').scrollIntoView({ behavior: 'smooth' });
+});
 
 
 const episodes = [
